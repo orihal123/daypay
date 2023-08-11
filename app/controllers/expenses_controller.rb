cb9e77216d178a2116a88af5581a8ff45b4d3332
orihal123 <138180298+orihal123@users.noreply.github.com>
2023-08-11 16:50:52 +0900

    支出機能の実装

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,2 +1,17 @@
 class ExpensesController < ApplicationController
+
+  def new
+    @expense = Expense.new
+  end
+  
+  def create
+    @expense = Expense.create(expense_params)
+    redirect_to '/'
+  end
+
+  private
+
+  def expense_params
+    params.require(:expense).permit(:expense_amount, :date, :category_id)
+  end
 end
