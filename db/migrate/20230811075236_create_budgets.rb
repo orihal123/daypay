dffd1afdf9cc6c8989ffd68293ace02a2bbab9b9
orihal123 <138180298+orihal123@users.noreply.github.com>
2023-08-11 17:11:37 +0900

    予算登録機能の実装

---
+++ db/migrate/20230811075236_create_budgets.rb
@@ -0,0 +1,10 @@
+class CreateBudgets < ActiveRecord::Migration[7.0]
+  def change
+    create_table :budgets do |t|
+      t.integer      :budget_amount,               null: false
+      t.date        :date,                       null: false
+
+      t.timestamps
+    end
+  end
+end
