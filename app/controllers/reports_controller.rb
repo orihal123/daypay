ffcd9a58bf94bbcc2d129b0e05273d4242b1f693
orihal123 <138180298+orihal123@users.noreply.github.com>
2023-08-14 17:08:19 +0900

    微調整

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,15 +1,52 @@
 class ReportsController < ApplicationController
-  def index
-    @incomes = Income.all
-    @expenses = Expense.all
-    today = Date.today
-    @budgets = Budget.all
-    @today_budget = Budget.where(date: Date.today).pluck(:budget_amount).first
+  
 
-    # 今日の日付の支出の合計を計算
+  def index
+    today = Date.today
+
+    # 今日の支出の合計を計算
     @total_expense_amount_today = Expense.where(date: today).sum(:expense_amount)
+
+    # 収入と支出をそれぞれ日付に基づいて取得
+    @incomes = Income.where(date: today)
+    @expenses = Expense.where(date: today)
+
+    # カレンダー表示のための日付と支出データを用意
+    @calendar_data = {}
+    @calendar_budgets = {}
+
+    # 支出データを日付ごとに合計してカレンダーにセット
+    Expense.where(date: today.beginning_of_month..today.end_of_month).group(:date).sum(:expense_amount).each do |date, expense_amount|
+      @calendar_data[date] = expense_amount
+    end
+
+    # 予算の登録日から月末まで登録額を等分して表示
+    @budgets = Budget.where(date: today.beginning_of_month..today.end_of_month)
+    if @budgets.present?
+      total_budget = @budgets.sum(:budget_amount)
+      days_in_month = (today.end_of_month.day - @budgets.first.date.day + 1)
+      @budget_per_day = (total_budget.to_f / days_in_month).to_i  # 小数点以下を切り捨て
+
+      # 予算の登録日以前の日は０、登録日から月末まで等分して表示
+      (today.beginning_of_month..today.end_of_month).each do |date|
+        if date < @budgets.first.date
+          @calendar_budgets[date] = 0
+        else
+          @calendar_budgets[date] = @budget_per_day
+        end
+      end
+    else
+      # 予算が登録されていない場合、全ての日に0を設定
+      (today.beginning_of_month..today.end_of_month).each do |date|
+        @calendar_budgets[date] = 0
+      end
+    end
   end
 
+
+  
   def create
   end
 end
+
+
