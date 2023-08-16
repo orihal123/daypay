8749154a606fb8f3124b3af2c88428092254cde0
orihal123 <138180298+orihal123@users.noreply.github.com>
2023-08-16 17:48:34 +0900

    incomeの削除

--- app/models/income.rb
+++
@@ -1,6 +0,0 @@
-class Income < ApplicationRecord
-  extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to_active_hash :category
-
-  validates :income_amount, :date, :category_id, presence: true
-end
