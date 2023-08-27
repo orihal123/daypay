31344fc3a900b54e26ed4a5f0c7fe405b749286b
orihal123 <138180298+orihal123@users.noreply.github.com>
2023-08-27 13:44:03 +0900

    整理

--- app/models/report.rb
+++
@@ -1,9 +0,0 @@
-class Report < ApplicationRecord
-  has_many :expenses
-  has_many :budgets
-  has_many :expense_days
-
-  extend ActiveHash::Associations::ActiveRecordExtensions
-
-  belongs_to_active_hash :category2
-end
